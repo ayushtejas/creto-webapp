94b89fbdeb3b8d578a45d687897ca089314f26de
shariquehaider <haider.sharique4@gmail.com>
2021-10-23 17:19:28 +0530

    latest

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,32 +1,32 @@
 import React from "react";
+import { FaGoogle, FaFacebook } from "react-icons/fa";
 
 
 function LoginForm(){
   return(
     <div>
-    <div class="login-container text-c animated flipInX">
-                <div>
-                    <h1 class="logo-badge text-whitesmoke"><span class="fa fa-user-circle"></span></h1>
-                </div>
-                    <h3 class="text-whitesmoke">Sign In Template</h3>
-                    <p class="text-whitesmoke">Sign In</p>
-                <div class="container-content">
-                    <form class="margin-t">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Username" required=""/>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="*****" required=""/>
-                        </div>
-                        <button type="submit" class="form-button button-l margin-b">Sign In</button>
-
-                        <a class="text-darkyellow" href="#"><small>Forgot your password?</small></a>
-                        <p class="text-whitesmoke text-center"><small>Do not have an account?</small></p>
-                        <a class="text-darkyellow" href="#"><small>Sign Up</small></a>
-                    </form>
-                    <p class="margin-t text-whitesmoke"><small> Your Name &copy; 2018</small> </p>
-                </div>
-            </div>
+    <div className="form">
+    <div className="form-container">
+      <div className="login-h1">
+      <h1>Login</h1>
+      </div>
+      <div className="loginform">
+        <div className="input-form">
+          <input type="text" name="" placeholder="Enter Username or Email"/>
+        </div>
+        <div className="input-form">
+        <input type="password" name="" placeholder="Enter Password"/>
+        </div>
+        <div className="login-btn">
+          <button type="submit" >Login</button>
+        </div>
+      </div>
+      <div className="oAuth-btn">
+        <a href="">< FaGoogle /></a>
+        <a href="">< FaFacebook /></a>
+      </div>
+      </div>
+    </div>
 	</div>
   )
 };
