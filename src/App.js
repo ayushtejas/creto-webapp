9910717cb0e53f51b7075c95da7bb456a203a8af
shariquehaider <haider.sharique4@gmail.com>
2021-11-11 14:43:03 +0530

    product

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from "./Components/Pages/Index";
 import Login from "./Components/Pages/Login";
 import Signup from "./Components/Pages/Signup";
+import Product from "./Components/Pages/product";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 
@@ -15,6 +16,7 @@ function App() {
           <Route exact path='/' component={Home}></Route>
           <Route exact path='/login' component={Login}></Route>
           <Route exact path='/signup' component={Signup}></Route>
+          <Route exact path='/product' component={Product}></Route>
       </Switch>
       </Router>
     </>
---
+++ src/Components/Pages/product.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Css/index.css";
+import Header from "../Header";
+import Footer from "../Footer";
+import Banner from "../Banner";
+import Products from "../Products";
+import About from "../About";
+import Blog from "../Blog";
+
+function Product(){
+  return(
+    <div>
+    < Header />
+    < Footer />
+    </div>
+  )
+}
+
+export default Products;
